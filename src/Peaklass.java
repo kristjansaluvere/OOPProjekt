91ffcf576f07397ea6db280a88d65ee7834b7735
Kristjan Saluvere <kristjansaluvere@ut.ee>
2026-05-11 19:28:35 +0300

    TLisasin erindeid, lisasin ajaloo faili ja täiustasin tähtkuju sisendit

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,4 +1,5 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,7 +13,7 @@ public class Peaklass {
         // Kuvab veateate, kui faili, mida avada ei leita.
         try {
             andmeHoidla.loeFailist();
-        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
             System.out.println("Viga: faili horoskoobid.csv ei leitud.");
             return;
         }
@@ -28,8 +29,9 @@ public class Peaklass {
         System.out.print("Sisesta oma nimi: ");
         String nimi = scanner.nextLine().trim();
 
-        System.out.print("Sisesta oma tähtkuju: ");
-        String tähtkuju = scanner.nextLine().trim();
+        kuvaTähtkujud();
+        String tähtkuju = küsiTähtkuju(scanner);
+        System.out.println();
 
         int meeleoluValik = küsiMeeleoluValik(scanner);
         String meeleolu = Kasutaja.teisendaMeeleolu(meeleoluValik);
@@ -47,6 +49,56 @@ public class Peaklass {
         System.out.println();
         System.out.println("----- Sinu horoskoop -----");
         System.out.println(horoskoop);
+
+        // Ajalugu/eelnevad ennustused
+        try {
+            andmeHoidla.salvestaAjalugu(kasutaja.getNimi(), horoskoop.getSõnum());
+            System.out.println("Horoskoop salvestatud.");
+        } catch (IOException e) {
+            System.out.println("Viga: ei saanud ajalukku salvestada.");
+        }
+
+        System.out.println("\nKas soovite näha varasemaid ennustusi? (jah/ei): ");
+        String vastus = scanner.nextLine().trim().toLowerCase();
+
+        if (vastus.equals("jah")) {
+            try {
+                System.out.println(andmeHoidla.loeAjalugu(kasutaja.getNimi()));
+            } catch (IOException e) {
+                System.out.println("Viga: ei saanud ajalugu failist lugeda.");
+            }
+        }
+        scanner.close();
+    }
+
+    // Tähtkuju valikud
+    private static void kuvaTähtkujud() {
+        String[] tähtkujud = {"Jäär", "Sõnn", "Kaksikud", "Vähk", "Lõvi", "Neitsi", "Kaalud", "Skorpion",
+                "Ambur", "Kaljukits", "Veevalaja", "Kalad"};
+        System.out.println("Vali oma tähtkuju:");
+        for (int i = 0; i < tähtkujud.length; i++) {
+            System.out.println((i+1) + " - " + tähtkujud[i]);
+        }
+    }
+
+    private static String küsiTähtkuju(Scanner scanner) {
+        String[] tähtkujud =  {"Jäär", "Sõnn", "Kaksikud", "Vähk", "Lõvi", "Neitsi", "Kaalud", "Skorpion",
+                "Ambur", "Kaljukits", "Veevalaja", "Kalad"};
+        while (true) {
+            System.out.println("Sisesta valik (1-12): ");
+            String sisend = scanner.nextLine().trim();
+
+            try {
+                int valik = Integer.parseInt(sisend);
+                if (valik >= 1 && valik <= 12) {
+                    return tähtkujud[valik - 1];
+                } else {
+                    System.out.println("Viga: vali õige number.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Viga: peab sisestama numbri. Proovi uuesti.");
+            }
+        }
     }
 
     // Küsime kasutajalt meeleolu nii kaua, kui sisestatakse õige valik.
@@ -58,20 +110,19 @@ public class Peaklass {
             System.out.println("3 - Neutraalne");
             System.out.print("Sisesta valik (1-3): ");
 
-            if (scanner.hasNextInt()) {
-                int valik = scanner.nextInt();
-                scanner.nextLine();
+            String sisend = scanner.nextLine().trim();
+
+            try {
+                int valik = Integer.parseInt(sisend);
 
                 if (valik >= 1 && valik <= 3) {
                     return valik;
+                } else {
+                    System.out.println("Viga: vali number vahemikus 1 kuni 3.");
                 }
-            } else {
-                scanner.nextLine();
+            } catch (NumberFormatException e) {
+                System.out.println("Viga: peab sisestama numbri! Proovi uuesti.");
             }
-
-            // Kui kasutaja sisestab vale valiku, siis kuvame teate.
-            System.out.println("Vigane valik. Proovi uuesti.");
-            System.out.println();
         }
     }
 
@@ -88,18 +139,22 @@ public class Peaklass {
         while (true) {
             System.out.print("Vali kategooria numbri järgi: ");
 
-            if (scanner.hasNextInt()) {
-                int valik = scanner.nextInt();
-                scanner.nextLine();
+            String sisend = scanner.nextLine().trim();
+
+            try {
+                int valik = Integer.parseInt(sisend);
 
                 if (valik >= 1 && valik <= kategooriad.size()) {
                     return kategooriad.get(valik - 1);
+                } else {
+                    System.out.println("Viga: Sellise numbriga kategooriat ei ole nimekirjas.");
                 }
-            } else {
-                scanner.nextLine();
+            } catch (NumberFormatException e) {
+                System.out.println("Viga: sisesta õige kategooria number.");
             }
 
-            System.out.println("Vigane kategooria valik. Proovi uuesti.");
         }
     }
+
+
 }
